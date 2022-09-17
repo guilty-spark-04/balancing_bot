1d4b6a6bc01c43d6ed284bce8a112d94b61b0329
Carlos <cespinal101@gmail.com>
2022-09-17 14:09:02 -0500

    basic movement

--- pid.py
+++ pid.py
@@ -1,6 +1,6 @@
 from colorTracking_v3 import *
 from adafruit_servokit import ServoKit
-
+import time
 pca = ServoKit(channels=16)
 servo1 = pca.servo[0]
 servo2 = pca.servo[1]
@@ -9,9 +9,11 @@ servo3 = pca.servo[2]
 
 while(1):
     x, y = get_coordinates()
+    print(f"X coordinate is: {x} \nY coordinate is: {y}")
     if(x > 0):
         servo1.angle = 180
     elif(x<0): servo1.angle = 0
     if(y >0):
         servo2.angle = 180
     elif(y<0):servo2.angle = 0
+    time.sleep(0.5)
