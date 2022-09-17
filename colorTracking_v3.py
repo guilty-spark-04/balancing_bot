336ffc17cbcabbe1e96855a6d0625c01e4f4dc7a
usernombre7@gmail.com <47437407+laurenkerno@users.noreply.github.com>
2022-09-17 12:40:42 -0500

    working cv2

--- colorTracking_v3.py
+++ colorTracking_v3.py
@@ -18,24 +18,22 @@ greenUpper = (64, 255, 255)
 
 pts = deque(maxlen=args["buffer"])
 counter = 0
-(dX, dY) = (0, 0)
-direction = ""
 
-if not args.get("video", False):
-	vs = VideoStream(src=0).start()	#Use to src-1 to use the second camera
-
-else:
-	vs = cv2.VideoCapture(args["video"])
+print(args)
+vs = cv2.VideoCapture(0)
 
 time.sleep(2.0)
 
 while True:
-	frame = vs.read()
+	_, frame = vs.read()
 	frame = frame[1] if args.get("video", False) else frame
 	if frame is None:
 		break
-	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	frame = frame[0:480,0:480]
+	width = 480
+	height = 480
+
+	blurred = cv2.GaussianBlur(frame, (11, 11),0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 	mask = cv2.inRange(hsv, greenLower, greenUpper)
 	mask = cv2.erode(mask, None, iterations=2)
@@ -52,27 +50,9 @@ while True:
 			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
 			pts.appendleft(center)
-			print("center: ", x," ", y)
-	for i in np.arange(1, len(pts)):
-		if pts[i - 1] is None or pts[i] is None:
-			continue
-		if counter >= 10 and i == 10 and pts[i-10] is not None:
-			dX = pts[i-10][0] - pts[i][0]
-			dY = pts[i-10][1] - pts[i][1]
-			(dirX, dirY) = ("", "")
-			if np.abs(dX) > 20:
-				dirX = "East" if np.sign(dX) == 1 else "West"
-			if np.abs(dY) > 20:
-				dirY = "North" if np.sign(dY) == 1 else "South"
-			if dirX != "" and dirY != "":
-				direction = "{}-{}".format(dirY, dirX)
-			else:
-				direction = dirX if dirX != "" else dirY
-		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		#Stop drawing the line
-		#cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-	cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,0.65, (0, 0, 255), 3)
-	cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+			cv2.putText(frame, "x: {}, y: {}".format(round(x)-width/2, (-1*(round(y)-height/2))),(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+	cv2.line(frame, (240,0), (240,480), (0, 0, 255), 1)
+	cv2.line(frame, (0,240), (480,240), (0, 0, 255), 1)
 	cv2.imshow("Frame", frame)
 	cv2.imshow("Mask", mask)		#Display mask window
 	key = cv2.waitKey(1) & 0xFF
